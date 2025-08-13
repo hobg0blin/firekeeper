9b0635a3df41832a1187a4e2877a2502097463a5
Brent Bailey <bb2906@nyu.edu>
2025-08-13 14:18:46 -0400

    fighting with WSL

--- firekeeper.py
+++ firekeeper.py
@@ -2,34 +2,66 @@ from ultralytics import YOLO
 import cv2
 from ultralytics.utils.plotting import Annotator
 
-model = YOLO("yolo11n.pt")
-
-cap = cv2.VideoCapture(1)
-cap.set(3, 650)
-cap.set(4, 480)
 
 
-while True:
-    _, img = cap.read()
-    
-    # BGR to RGB conversion is performed under the hood
-    # see: https://github.com/ultralytics/ultralytics/issues/2575
-    results = model.predict(img)
 
-    for r in results:
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 6: # if there are more than 5 non working ports stop the testing. 
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." %dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not read." %(dev_port,h,w))
+                available_ports.append(dev_port)
+        dev_port +=1
+    return available_ports,working_ports,non_working_ports
+
+def run_demo():
+    # USB camera on WSL2
+    model = YOLO("yolo11n.pt")
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+    assert cap.isOpened(), "Camera not found. Please check the camera connection."
+
+    while True:
+        _, img = cap.read()
         
-        annotator = Annotator(img)
-        
-        boxes = r.boxes
-        for box in boxes:
+        # BGR to RGB conversion is performed under the hood
+        # see: https://github.com/ultralytics/ultralytics/issues/2575
+        results = model.predict(img)
+
+        for r in results:
             
-            b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
-            c = box.cls
-            annotator.box_label(b, model.names[int(c)])
-          
-    img = annotator.result()  
-    cv2.imshow('YOLO 11 Detection', img)     
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        break
+            annotator = Annotator(img)
+            
+            boxes = r.boxes
+            for box in boxes:
+                
+                b = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+                c = box.cls
+                annotator.box_label(b, model.names[int(c)])
+              
+        img = annotator.result()  
+        cv2.imshow('YOLO 11 Detection', img)     
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            break
 
+list_ports()
 
+run_demo()
